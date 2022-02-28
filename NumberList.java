feb9168065d35207d806b9c9a8d6948ab5ef1eff
Odwambombo <99673174+Odwambombo@users.noreply.github.com>
2022-02-28 19:53:41 +0200

    Update NumberList.java

--- NumberList.java
+++ NumberList.java
@@ -27,6 +27,7 @@ public class NumberList implements NumberRangeSummarizer {
 	 
 	
 
+	// Get input and strore in an ArrayList
 	@Override
 	public Collection<Integer> collect(String input) {
     Collection<Integer> sorted = new ArrayList<>();
@@ -45,18 +46,21 @@ public class NumberList implements NumberRangeSummarizer {
 		
 		 String finalstring = "";
 		 
+		 // Removes brackets and spaces from ArrayList and convert it to a strings
 		 String new_input = input.toString();
 		 String str = new_input.replaceAll("\\[(.*?)\\]", "$1");
 		 str = str.replaceAll("\\s+", "");
 		 
+		 // Coverts String to an Array by separating the values by a comma
 		 int[] a = Arrays.stream(str.split(",")).map(Integer::parseInt).mapToInt(i -> i).toArray();
 		 
-		 int count=0;
-		 int start=a[0];
-		 String temp=""+a[0];
-		 int end=a[0];
+		 int count=0; // counter that keeps track of how many numbers are sequential
+		 int start=a[0]; //first value of a sequential pattern
+		 String temp=""+a[0]; //String that stores the current pattern
+		 int end=a[0]; // Last value of sequential pattern
 		 
 		 for (int i = 0; i <a.length -1; i++){
+			 //check  if value is sequential
 			 if (a[i + 1] == a[i]+1){
 				 
 				 if (count == 0){
@@ -66,8 +70,9 @@ public class NumberList implements NumberRangeSummarizer {
 				 count++;
 				 }
 			 
-			 
+			 // Else value is not sequential
 			 else {
+				
 				 if (count>0){
 					 count = 0;
 					 temp = start+ "-"+ end;
@@ -77,7 +82,7 @@ public class NumberList implements NumberRangeSummarizer {
 				 
 			 }
 		 }
-		 
+		 // check if final value is part of a sequential pattern
 		 if (count>0){
 			 temp = start + "-" + end;
 		 }
